aca2359a113d59aa2c4959ed6f9015b97706e96f
nikmat1996 <nikhilmathew1996@gmail.com>
2024-07-04 09:39:48 +0530

    feat: integrate ShadCN into the project

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,13 @@
 import './app.css'
+import { Button } from './components/ui/button'
 
 export function App() {
 
   return (
     <>
-      <h1 className="text">Hello NM</h1>
+      <h1 className="text-red-400">Hello world</h1>
+      <Button variant={'outline'}>shad test button</Button>
     </>
   )
 }
+
